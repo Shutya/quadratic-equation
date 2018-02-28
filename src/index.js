a20e0ab8e207ebae3f26ac96a0c0d8eef24d1b0b
Dzianis Shutikov <Dzianis_Shutikov@epam.com>
2018-03-01 01:00:51 +0300

    Solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const numbers = equation.match(/(-?|\+\s|-\s)[^\^][0-9]+/g).map(item => Number(item.replace(/\s/g, '')));
+
+  const [a, b, c] = numbers;
+
+  const D = Math.pow(b, 2) - 4 * a * c;
+
+  const res1 = (-b + Math.sqrt(D)) / (2 * a);
+  const res2 = (-b - Math.sqrt(D)) / (2 * a);
+
+  const result = [Math.round(res1), Math.round(res2)];
+
+  return result.sort((a, b) => a - b);
 }
